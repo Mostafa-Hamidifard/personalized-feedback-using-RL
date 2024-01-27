27d0de68f464f9d504bdfdab6158ac00dbc489c0
Mostafa Hamidifard <79892356+Mostafa-Hamidifard@users.noreply.github.com>
2024-01-27 15:33:07 +0330

    skin.py code completed
    just we have some problem about generation of a and b.

--- Human.py
+++ Human.py
@@ -15,18 +15,27 @@ class Human():
 
 
     class Skin():
-        def __init__(self):
-            self.a = 0 #modify
-            self.b = 0 #modify
+        def __init__(self,num_vibrators,interDistance,options = None):            
+            if options != None:
+                pass            
+
+            self.num_vibrators = num_vibrators
+            self.interDistance = interDistance
+            # The question here is that why these numbers?
+            self.a = np.random.uniform(1,8,size=(num_vibrators,))
+            self.b = np.random.uniform(0.4,2,size=(num_vibrators,)) #modify
             self.counter = 0
             self.delay = 5
-        
+            
+            
         def __call__(self, inp):
             if self.counter < self.delay:
                 self.counter += 1
             else:
                 self.counter = 0
-                return self.a * np.exp(self.b * inp) #modify
+                # be aware that inp must convert to a 1d vector
+                inp = inp.reshape((-1,))
+                return self.a * np.exp(self.b * inp) 
     
     
     class Brain():
