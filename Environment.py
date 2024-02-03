d19b7f58a084777336966089f3491d1ed437fda2
Mostafa Hamidifard <79892356+Mostafa-Hamidifard@users.noreply.github.com>
2024-02-03 11:51:27 +0330

    human completed. monitor is incomplete.

--- Environment.py
+++ Environment.py
@@ -1,19 +1,26 @@
 from Human import Human
 from EMG_Interpretation import EMG_Interpretation
 import numpy as np
-from matplotlib import pyplot as plt
+import pygame
+# import Threading
+# import sys
 
 DIM = 240
 X = 0
 Y = 1
 
 class Environment:
-    def __init__(self):
+    def __init__(self,dt=0.0001):
         self.pos = np.zeros((2))
         self.dest = (np.random.rand(2)*2*DIM) - DIM
+        
+  
+
         self.human = Human()
         self.monitor = self.Monitor()
         self.vibrator = self.Vibrator(k=2)
+    
+    
     
     def __call__(self, action):
         f = self.vibrator(action)
@@ -29,21 +36,65 @@ class Environment:
     def reset(self):
         self.__init__()
 
-    class Monitor():
-        def __init__(self):
-            pass
-        
-        def __call__(self, pos, dest):
-            fig, ax = plt.subplots(figsize=(10,5))
 
-            ax.scatter(dest[X], dest[Y], marker='o', color='r')
-            ax.scatter(pos[X], pos[Y], marker='o', color='b')
-            plt.xlim((-DIM,DIM))
-            plt.ylim((-DIM,DIM))
-            ax.set_xlabel('X')
-            ax.set_ylabel('Y')
-            ax.set_title('Map')
-            plt.show()
+    class dynamical_model:
+        def __init__(self,initial_position,dt):
+            
+            self.p = initial_position.reshape((-1,))
+            self.dt = dt
+            
+        def update(self,input_u):
+            self.p = self.p + self.dt * input_u
+            return self.p
+            
+        def get_position(self):
+            return self.p
+
+    class Monitor():
+        def __init__(self,render_mode=None):
+            self.render_mode = render_mode
+            self.is_window_displaying = False
+            self.p_current = np.array([0,0])
+            self.p_desired = np.array([0,0])
+            self.circle_radius = 0
+            self.desired_rect = None
+            self.current_rect = None
+            self.show_desired = True
+            
+        def turn_off_monitor(self):
+            if self.is_window_displaying:
+                pygame.quit()
+                self.is_window_displaying = False
+        
+        def turn_on_monitor(self,width=480,height=480):
+            if self.render_mode != 'human':
+                print("render_mode is not set to <human>.")
+                return 0
+            pygame.init()
+            self.window = pygame.display.set_mode((width,height))
+            self.is_window_displaying = True
+            
+            self.circle_radius = width//20
+            
+            def draw_thread():
+                p_current = self.p_current
+                p_desired = self.p_desired
+                if self.show_desired == True:    
+                    self.desired_rect =  pygame.draw.circle(self.window, (255,0,0), p_desired[0],p_desired[1], self.circle_radius)
+                self.current_rect = pygame.draw.circle(self.window, (0,0,255), p_current[0],p_current[1], self.circle_radius)
+                
+            def runnimg_loop():
+                running = True
+                while self.is_window_displaying and running:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            running = False  
+                            # sys.exit("window was closed")
+                self.turn_off_monitor()
+            
+                
+        def __call__(self, pos, dest):
+            pass
     
     
     class Vibrator():
--- Human.py
+++ Human.py
@@ -13,7 +13,6 @@ class Human():
         return self.emg(intention)
 
 
-
     class Skin():
         """angles is a tuple which contains the angles corrosponding to the vibrator positions computed CCW
         from right arm.
@@ -21,7 +20,8 @@ class Human():
         def __init__(self,num_vibrators,angles=None,options = None):            
             if options != None:
                 pass            
-            
+            self.counter = 0
+            self.delay = 0
             self.num_vibrators = num_vibrators
             self.angles = angles
 
@@ -47,8 +47,8 @@ class Human():
     class Brain():
         def __init__(self):
             self.counter = 0
-            self.delay = 20
-            self.target_pos = np.zeros((2,))
+            self.delay = 4
+            self.computed_velocity = np.zeros((2,))
         
         """
         It is assumed that vibrations are distributed around human body,
@@ -65,31 +65,67 @@ class Human():
                 percieved_error = percieved_error + stimulus[idx] * vec
             return percieved_error                
 
-        def __call__(self, stimulus,angles, pos):
+        def __call__(self, stimulus,angles):
             if self.counter < self.delay:
                 self.counter += 1
             else:
                 self.counter = 0
                 percieved_error = self._calculate_percieved_error(stimulus,angles)
-                self.target_pos = pos + percieved_error
+                self.computed_velocity = percieved_error
                 
-            return self.target_pos
+            return self.computed_velocity
     
     class EMG_Model():
-        def __init__(self):
+        def __init__(self,delta,emg_amp_max,cross_max_ratio=0.1):
             
+            self.emg_amp_max = emg_amp_max
+            cross_max = cross_max_ratio * self.emg_amp_max
+            self.alpha = cross_max / np.log(self.emg_amp_max + 1)
+            
+            self.delta = delta 
             self.counter = 0
-            self.delay = 5
-        
+            self.delay = 0
+            self.u = np.zeros((4,))
+            self.mapper = {"up":0,"down":1,"left":2,"right":3}
+            
+        def _u2emg(self,u_list):
+            t = self.counter * self.delta
+            w0 = 2*3.14*200;
+            emg_list = np.zeros_like(u_list)
+            for i,u in enumerate(u_list):  
+                n1 = 0.1 * np.random.randn() # these paramters should get adapted
+                n2 = 5 * np.random.randn()
+                n3 = 0.05 * np.random.randn()
+                n4 = 5  * np.random.randn()
+                emg_list[i] = (u+n1)*np.sin( (w0+n2) * t + n4) + n3
+                
+
+        """BE aware that it shouldnt get delayed"""
         def __call__(self, intention):
-            if self.counter < self.delay:
-                self.counter += 1
+            self.counter += 1
+            
+            v_x = intention[0]
+            v_y = intention[1]
+            
+            if v_x >= 0:
+                self.u[self.mapper["right"]] = min(v_x,self.emg_amp_max)
+                self.u[self.mapper["left"]] = self.alpha * np.log(self.u[self.mapper["right"]] + 1)
             else:
-                self.counter = 0
-                return intention
+                self.u[self.mapper["left"]] = min(-v_x,self.emg_amp_max)
+                self.u[self.mapper["right"]] = self.alpha * np.log(self.u[self.mapper["left"]] + 1)
+            
+            if v_y >=0:
+                self.u[self.mapper["up"]] = min(v_y,self.emg_amp_max)
+                self.u[self.mapper["down"]] = self.alpha * np.log(self.u[self.mapper["up"]] + 1)
+            else:
+                self.u[self.mapper["down"]] = min(-v_y,self.emg_amp_max)
+                self.u[self.mapper["up"]] = self.alpha * np.log(self.u[self.mapper["down"]] + 1)
+            
+            return {"modeled_emg": self._u2emg(self.u),"original": self.u}    
+            
+                
 
 if __name__ == "__main__":
     human = Human(4)
     for i in range(100):
-        print(i)
-        print(human.brain([1,0,1,2*i],human.skin.angles,0))
+        pass
